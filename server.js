e299e30c7380f0427f03df7605da4f7800e74177
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-06-25 16:06:06 +0530

    init2

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-const { clear } = require('console')
 const http=require('http')
 
 
 function reqListener(req,res){
-  console.log("My name is Piyush")
+   const url=req.url
+   console.log(url)
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+   if(url==='/home')  res.write("Welcome home")
+   else if(url==='/about') res.write("Welcome to About Us")
+   else if(url==='/node') res.write("Welcome to my Node-js Project")
+   else res.write("Page does'nt exist")
+  res.end()
 }
 
 
 const server=http.createServer(reqListener)
 
 
-server.listen(4000)
+server.listen(4000,()=>console.log("server started"))
 
 
 
